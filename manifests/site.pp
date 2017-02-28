b804b2d016f769483440f74980b37d3aaa3eee7d
drewbilee <drewbilee@puppetlabs.vm>
2017-02-28 20:56:14 +0000

    demonstrating site.pp node classification

--- manifests/site.pp
+++ manifests/site.pp
@@ -22,7 +22,10 @@ Host <<| tag == 'puppetlabs' |>>
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-
+node 'drewbilee.puppetlabs.vm' {
+  include role::classroom
+  notify { 'this node is getting node-specific configuration': }
+}
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
