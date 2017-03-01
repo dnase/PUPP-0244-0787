c211a8bcc5fe3997cd0bb8b6f08a5bef3510991c
drewbilee <drewbilee@puppetlabs.vm>
2017-03-01 19:39:32 +0000

    lab 10.2 - platform abstraction

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,31 +1,52 @@
 class nginx {
+  case $::osfamily {
+    'redhat', 'debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx/conf'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+    }
+    default : {
+      fail("Unsupported OS (${::osfamily}) detected!")
+    }
+  }
   File {
-    owner  => 'root',
-    group  => 'root',
+    owner  => $owner,
+    group  => $group,
     mode   => '0644',
   }
-  package { 'nginx':
+  package { $package:
     ensure => latest,
     before => File['nginx.conf', 'default.conf'],
   }
   file { 'docroot':
     ensure => directory,
-    path   => '/var/www',
+    path   => $docroot,
   }
   file { 'index.html':
     ensure => file,
-    path   => '/var/www/index.html',
+    path   => "${docroot}/index.html",
     source => 'puppet:///modules/nginx/index.html',
   }
   file { 'nginx.conf':
     ensure => file,
-    path   => '/etc/nginx/nginx.conf',
-    source => 'puppet:///modules/nginx/nginx.conf',
+    path   => "${confdir}/nginx.conf",
+    source => "puppet:///modules/nginx/${::osfamily}.conf",
   }
   file { 'default.conf':
     ensure => file,
-    path   => '/etc/nginx/conf.d/default.conf',
-    source => 'puppet:///modules/nginx/default.conf',
+    path   => "${blockdir}/default.conf",
+    source => "puppet:///modules/nginx/default-${::kernel}.conf",
   }
   service { 'nginx':
     ensure    => running,
