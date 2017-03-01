c06a5977fc6751f3487a6144eb1553e833c1119d
drewbilee <drewbilee@puppetlabs.vm>
2017-03-01 19:01:07 +0000

    lab 10.1 - refactor with resource defaults

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,37 +1,30 @@
 class nginx {
+  File {
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
   package { 'nginx':
     ensure => latest,
     before => File['nginx.conf', 'default.conf'],
   }
   file { 'docroot':
     ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     path   => '/var/www',
   }
   file { 'index.html':
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     path   => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
   }
   file { 'nginx.conf':
     ensure => file,
     path   => '/etc/nginx/nginx.conf',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/nginx/nginx.conf',
   }
   file { 'default.conf':
     ensure => file,
     path   => '/etc/nginx/conf.d/default.conf',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/nginx/default.conf',
   }
   service { 'nginx':
