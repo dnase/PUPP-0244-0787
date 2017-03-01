098d0f459afd7f559312847f662eebbe305cfb5e
drewbilee <drewbilee@puppetlabs.vm>
2017-03-01 17:12:10 +0000

    lab 9.1 - package/file/service

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,6 +1,7 @@
 class nginx {
   package { 'nginx':
     ensure => latest,
+    before => File['nginx.conf', 'default.conf'],
   }
   file { 'docroot':
     ensure => directory,
@@ -16,5 +17,26 @@ class nginx {
     mode   => '0644',
     path   => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
+  }
+  file { 'nginx.conf':
+    ensure => file,
+    path   => '/etc/nginx/nginx.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
+  file { 'default.conf':
+    ensure => file,
+    path   => '/etc/nginx/conf.d/default.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['nginx.conf', 'default.conf'],
   }
 }
