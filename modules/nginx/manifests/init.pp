b3a1ac2047f01e443acbb8145b3fae4db815a84e
drewbilee <drewbilee@puppetlabs.vm>
2017-03-01 21:05:57 +0000

    lab 11.1 - dynamic content with templates

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -7,6 +7,7 @@ class nginx {
       $docroot = '/var/www'
       $confdir = '/etc/nginx'
       $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
     }
     'windows': {
       $package = 'nginx-service'
@@ -15,10 +16,16 @@ class nginx {
       $docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx/conf'
       $blockdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir = 'C:/ProgramData/nginx/logs'
     }
     default : {
       fail("Unsupported OS (${::osfamily}) detected!")
     }
+  }
+  $user = $::osfamily ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
   }
   File {
     owner  => $owner,
@@ -34,19 +41,24 @@ class nginx {
     path   => $docroot,
   }
   file { 'index.html':
-    ensure => file,
-    path   => "${docroot}/index.html",
-    source => 'puppet:///modules/nginx/index.html',
+    ensure  => file,
+    path    => "${docroot}/index.html",
+    content => epp('nginx/index.html.epp'),
   }
   file { 'nginx.conf':
-    ensure => file,
-    path   => "${confdir}/nginx.conf",
-    source => "puppet:///modules/nginx/${::osfamily}.conf",
+    ensure     => file,
+    path       => "${confdir}/nginx.conf",
+    content    => epp('nginx/nginx.conf.epp', {
+      user     => $user,
+      logdir   => $logdir,
+      confdir  => $confdir,
+      blockdir => $blockdir,
+    }),
   }
   file { 'default.conf':
-    ensure => file,
-    path   => "${blockdir}/default.conf",
-    source => "puppet:///modules/nginx/default-${::kernel}.conf",
+    ensure  => file,
+    path    => "${blockdir}/default.conf",
+    content => epp('nginx/default.conf.epp', {docroot => $docroot}),
   }
   service { 'nginx':
     ensure    => running,
